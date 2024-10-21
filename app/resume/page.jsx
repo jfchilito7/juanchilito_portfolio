30b8fab912726f9f1a671988f52f3d0c249fca28
jfchilito7 <jfchilito7@gmail.com>
2024-10-21 16:20:45 -0500

    creacion de apartado resumen con sus respectivas subcategorias

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -28,20 +28,20 @@ const about = {
       fieldValue: "Colombia🇨🇴"
     },
     {
-      fieldName: "GitHub",
-      fieldValue: "https://github.com/jfchilito7"
-    },
-    {
       fieldName: "Email",
       fieldValue: "jfchilito7@gmail.com"
+    },
+    {
+      fieldName: "Lenguajes",
+      fieldValue: "Español, Inglés-B2"
     },
     {
       fieldName: "Freelance",
       fieldValue: "Disponible"
     },
     {
-      fieldName: "Lenguajes",
-      fieldValue: "Español, Inglés-B2"
+      fieldName: "GitHub",
+      fieldValue:"github.com/jfchilito7"
     },
   ]
 };
@@ -51,11 +51,222 @@ const expirience = {
   icon: '/assets/resume/badge.svg',
   title: "Mi Experiencia",
   description: "Soy un desarrollador web fullstack con experiencia en el desarrollo de aplicaciones web y móviles. Me encanta trabajar en proyectos desafiantes y aprender nuevas tecnologías.",
-}
+  items: [
+    {
+      company: "Bayron Becerra Foundation",
+      position: "Desarrollador De Contenido Multimedia",
+      duration: "Junio 2024 - Actualidad"
+    },
+    {
+      company: "Freelancer",
+      position: "Desarrollador Web freelance",
+      duration: "Octubre 2023 - Actualidad"
+    },
+    {
+      company: "Facultad Arquitectura Univalle",
+      position: "Desarrollador Web",
+      duration: "Junio 2023 - Agosto 2023"
+    },
+  ]
+};
+
+//education data
+const education = {
+  icon: '/assets/resume/cap.svg',
+  title: "Mi Educación",
+  description: "A continuación, se presentan mis estudios realizados hasta la fecha.",
+  items: [
+    {
+      institution: "Universidad Autónoma De Occidente, Cali",
+      degree: "Especialización Gerencia De Proyectos",
+      duration: "2022 - 2024"
+    },  
+    {
+      institution: "Universidad Autónoma De Occidente, Cali",
+      degree: "Ingeniero De Multimedia",
+      duration: "2017 - 2023"
+    }, 
+    {
+      institution: "Institución Educativa Instituto Técnico, Santander de Quilichao.",
+      degree: "Bachiller académico",
+      duration: "2011 - 2016"
+    }, 
+  ]
+};
+
+//skills data
+const skills = {
+  title: "Mis Habilidades",
+  description: " A continuación, se presentan las habilidades que he adquirido a lo largo de mi carrera como desarrollador.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS3",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <FaGithub />,
+      name: "GitHub",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <DiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <DiMysql />,
+      name: "MySQL",
+    },
+  ]
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 
 const Resume = () => {
-  return <div>Resume</div>
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn"}
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs 
+        defaultValue="experience"
+        className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experiencia</TabsTrigger>
+            <TabsTrigger value="education">Educación</TabsTrigger>
+            <TabsTrigger value="skills">Habilidades</TabsTrigger>
+            <TabsTrigger value="about">Acerca de mi</TabsTrigger>
+          </TabsList>
+          {/* Content */}
+          <div className="min-h-[70vh] w-full">
+            {/* Experience */}
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{expirience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{expirience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {expirience.items.map((item, index) => {
+                      return (
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot*/}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            {/* education */}
+            <TabsContent value="education" className="w-full">
+            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot*/}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            {/* skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return <li key={index}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                          <div className="text-6xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p className="capitalize">{skill.name}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                    </li> 
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+            {/* about */}
+            <TabsContent value="about" className="w-full text-center xl:text-left">
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-7 max-w-[720px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                      <span className="text-white/60">{item.fieldName}</span>
+                      <span className="text-xl">{item.fieldValue}</span>
+                    </li>
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  )
 }
 
 export default Resume
